dfc7890f1dd907f92f3ce3000dee1d4b10c475c7
the projects <84352313+theorchidproject@users.noreply.github.com>
2023-01-31 01:15:20 +0000

    Update aosp_star2lte.mk

--- aosp_star2lte.mk
+++ aosp_star2lte.mk
@@ -29,7 +29,7 @@ TARGET_SCREEN_WIDTH := 1440
 TARGET_BOOT_ANIMATION_RES := 1440
 
 ## Inherit some common AOSP stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/proton/build/product.mk)
 
 ## Device identifier, this must come after all inclusions
 PRODUCT_NAME := aosp_star2lte
